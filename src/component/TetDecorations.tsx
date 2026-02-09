0e2638df15959f63f5f9ad1f33fd0ad0bfc89158
Felix Dinh <522h0028@duthu.net>
2026-02-09 13:54:38 +0700

    feat: Refine Tet decorations - transparent apricot blossom and better layout

--- src/component/TetDecorations.tsx
+++ src/component/TetDecorations.tsx
@@ -19,72 +19,67 @@ export default function TetDecorations() {
 
     return (
         <div className="fixed inset-0 pointer-events-none z-[40] overflow-hidden">
-            {/* Lantern Left */}
-            <div className="absolute top-[-10px] left-[10px] w-[60px] sm:w-[100px] z-50 tet-lantern" style={{ animationDelay: "0s" }}>
-                <Image
-                    src="/tet/lantern.png"
-                    alt="Lồng đèn"
-                    width={100}
-                    height={160}
-                    className="w-full h-auto drop-shadow-lg"
-                    priority
-                />
-            </div>
-
-            {/* Lantern Right */}
-            <div className="absolute top-[-20px] right-[10px] w-[50px] sm:w-[90px] z-50 tet-lantern" style={{ animationDelay: "1s" }}>
-                <Image
-                    src="/tet/lantern.png"
-                    alt="Lồng đèn"
-                    width={100}
-                    height={160}
-                    className="w-full h-auto drop-shadow-lg"
-                    priority
-                />
-            </div>
-
-            {/* Cherry Blossom Branch Top Right */}
-            <div className="absolute top-[-30px] right-[-20px] w-[200px] sm:w-[350px] opacity-90 z-40 transform rotate-[10deg]">
+            {/* Apricot Blossom Branch Top Left */}
+            <div className="absolute top-0 left-0 w-[180px] sm:w-[300px] z-40">
                 <Image
                     src="/tet/cherry.png"
-                    alt="Hoa đào"
-                    width={350}
-                    height={200}
-                    className="w-full h-auto object-contain"
-                    priority
-                />
-            </div>
-
-            {/* Cherry Blossom Branch Top Left (Flipped) */}
-            <div className="absolute top-[-20px] left-[-30px] w-[180px] sm:w-[300px] opacity-80 z-40 transform scale-x-[-1] rotate-[-10deg] hidden sm:block">
-                <Image
-                    src="/tet/cherry.png"
-                    alt="Hoa đào"
+                    alt="Hoa mai"
                     width={300}
-                    height={200}
+                    height={300}
                     className="w-full h-auto object-contain"
+                    priority
+                />
+            </div>
+
+            {/* Apricot Blossom Branch Top Right (Flipped) */}
+            <div className="absolute top-0 right-0 w-[180px] sm:w-[300px] z-40 transform scale-x-[-1]">
+                <Image
+                    src="/tet/cherry.png"
+                    alt="Hoa mai"
+                    width={300}
+                    height={300}
+                    className="w-full h-auto object-contain"
+                    priority
+                />
+            </div>
+
+            {/* Lantern Left (Adjusted position) */}
+            <div className="absolute top-[60px] left-[80px] sm:top-[100px] sm:left-[140px] w-[50px] sm:w-[80px] z-50 tet-lantern" style={{ animationDelay: "0s" }}>
+                <Image
+                    src="/tet/lantern.png"
+                    alt="Lồng đèn"
+                    width={100}
+                    height={160}
+                    className="w-full h-auto drop-shadow-lg"
+                    priority
+                />
+            </div>
+
+            {/* Lantern Right (Adjusted position) */}
+            <div className="absolute top-[60px] right-[80px] sm:top-[100px] sm:right-[140px] w-[40px] sm:w-[70px] z-50 tet-lantern" style={{ animationDelay: "1s" }}>
+                <Image
+                    src="/tet/lantern.png"
+                    alt="Lồng đèn"
+                    width={100}
+                    height={160}
+                    className="w-full h-auto drop-shadow-lg"
                     priority
                 />
             </div>
 
             {/* Horse Mascot Bottom Left */}
-            <div className="absolute bottom-[80px] left-[20px] w-[80px] sm:w-[120px] z-50 tet-mascot-float hidden sm:block">
+            <div className="absolute bottom-[20px] left-[20px] w-[100px] sm:w-[150px] z-50 tet-mascot-float hidden sm:block">
                 <Image
                     src="/tet/horse.png"
                     alt="Mascot Ngựa"
-                    width={120}
-                    height={120}
+                    width={150}
+                    height={150}
                     className="w-full h-auto drop-shadow-2xl"
                 />
                 <div className="bg-red-600 text-[#FFD700] text-[12px] font-bold px-3 py-1 rounded-full absolute -top-4 -right-4 border border-[#FFD700] shadow-lg animate-bounce whitespace-nowrap">
                     Chúc Mừng Năm Mới
                 </div>
             </div>
-
-            {/* Banner Center Top (Small Badge) */}
-            {/* <div className="absolute top-[80px] left-1/2 transform -translate-x-1/2 w-[200px] z-30 opacity-80 hidden sm:block">
-           <Image src="/tet/banner.png" alt="Tet 2026" width={300} height={100} className="w-full h-auto" />
-       </div> */}
 
             {/* Falling Petals */}
             {petals.map((petal) => (
